3b0df2bf3a0d80adbc72b3eb6377c25d3230fb2c
Vilde Kure <v-kure@hotmail.com>
2022-09-15 13:40:44 +0200

    fixes

--- app/src/main/java/com/example/ordlek/Fasit.java
+++ app/src/main/java/com/example/ordlek/Fasit.java
@@ -43,6 +43,5 @@ public class Fasit extends AppCompatActivity {
         ArrayAdapter<String> listeFunnetAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, ordListe);
         found.setAdapter(listeFunnetAdapter);
-
     }
 }
